d144a0040dbf86b4fbc6bea041b23f600e0b7023
KJB <drv98@naver.com>
2026-06-25 11:19:05 +0900

    오버레이 이미지

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,8 +2,12 @@ export default function MovieList({ movies }) {
   return (
     <>
       {movies.map((movie) => (
-        <div className="d-flex m-3" key={movie.imdbID}>
-          <img src={movie.Poster} alt="영화" />
+        <div className="image-container d-flex m-3" key={movie.imdbID}>
+          <img className="img-thumbnail" src={movie.Poster} alt="movie" />
+          <div className="overlay d-flex align-items-center justify-content-center">
+            <span className="me-2">선호작 추가</span>
+            <span>❤️</span>
+          </div>
         </div>
       ))}
     </>
